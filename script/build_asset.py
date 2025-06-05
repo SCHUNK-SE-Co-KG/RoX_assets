4e99d814e16fc1a33268d08a5814e4cae342761b
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-06-05 17:45:45 +0200

    Add a script for building RoX assets
    This currently creates a zip file for Intrinsic's FlowState from .obj
    files and an .sdf file.

---
+++ script/build_asset.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+import glob
+import os
+
+# Method         | Compression Level | Compatibility     | Speed
+# -------------- | ----------------- | ------------------| -------------
+# ZIP_STORED     | None              | Universal         | Fastest
+# ZIP_DEFLATED   | Medium            | Universal         | Fast
+# ZIP_BZIP2      | High              | Limited support   | Slower
+# ZIP_LZMA       | Highest           | Limited support   | Slowest
+from zipfile import ZipFile, ZIP_LZMA
+
+# Notes:
+# - ZIP_DEFLATED is the best balance of compression and compatibility.
+# - ZIP_LZMA gives the smallest file size but may not open on older systems.
+
+
+target = "../assets/egk_25.zip"
+
+
+def main(sdf_file: str, cad_folder: str) -> None:
+    with ZipFile(target, "w", compression=ZIP_LZMA) as f:
+
+        # .sdf
+        f.write(sdf_file, arcname=os.path.basename(sdf_file))
+
+        # .obj
+        obj_files = glob.glob(os.path.join(cad_folder, "*.obj"))
+        for file in obj_files:
+            f.write(file, arcname=os.path.basename(file))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Create a RoX asset.")
+    parser.add_argument("sdf_file", type=str, help="Path to the .sdf file.")
+    parser.add_argument(
+        "obj_folder", type=str, help="Path to the folder containing .obj files."
+    )
+    args = parser.parse_args()
+    main(args.sdf_file, args.obj_folder)
